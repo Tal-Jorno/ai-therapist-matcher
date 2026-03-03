2d95abb8433a2a0b8433eaf529510a57771fd8e5
Tal Jorno <taljor123@example.com>
2026-03-03 20:04:52 +0200

    g

--- app/db/deps.py
+++ app/db/deps.py
@@ -1,13 +1,19 @@
 from app.db.database import SessionLocal
 from app.logger_config import logger
+from fastapi import HTTPException
 
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
+        db.commit()
+    except HTTPException:
+        db.rollback()
+        raise
     except Exception:
-        logger.error("Database session error.", exc_info=True)
+        db.rollback()
+        logger.exception("Database session error.")
         raise
     finally:
         db.close()
